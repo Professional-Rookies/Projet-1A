97053e53d7b5052ed44bcbebfd47e07363baba38
dhiaabdelli1 <dhia.abdelli1@esprit.tn>
2020-06-02 14:14:45 +0100

    first

---
+++ src/jeu.h
@@ -0,0 +1,12 @@
+#ifndef PLAY_H_
+#define PLAY_H_
+#include <stdio.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_mixer.h"
+#include "SDL/SDL_ttf.h"
+#include "hero.h"
+#include "structs.h"
+#include "SDL/SDL_rotozoom.h"
+void jeu(SDL_Surface *ecran, etat *etat,hero *safwen,parameter *p,character c,background background,dialogue d);
+#endif
